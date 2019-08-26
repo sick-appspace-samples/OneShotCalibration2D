8e717ec90bb769ed40f83525eeec1ae00a92a552
Florian Roser <Florian.Roser@sick.de>
2019-08-26 18:36:37 +0200

    'Update'

--- OneShotCalibration2D/scripts/OneShotCalibration2D.lua
+++ OneShotCalibration2D/scripts/OneShotCalibration2D.lua
@@ -34,21 +34,22 @@ local DELAY = 2000
 
 -- Creating viewer
 local viewer = View.create()
-viewer:setID('viewer2D')
 
--- Creating header text and decoration attributes
+-- Text decoration object to display some feedback
 local text = View.TextDecoration.create()
-text:setPosition(25, 25) -- In pixels before origin is set in world coord
-text:setSize(40) -- In pixels before sizes are defined in mm
 text:setColor(0, 255, 0)
+text:setPosition(25, 50) -- In pixels before origin is set in world coord
+text:setSize(40) -- In pixels before sizes are defined in mm
 
 --End of Global Scope-----------------------------------------------------------
 
 --Start of Function and Event Scope---------------------------------------------
-
 local function main()
   local checkerBoard = Image.load('resources/pose.bmp') -- Calibration target
-  viewer:view(checkerBoard)
+  viewer:clear()
+  local imid = viewer:addImage(checkerBoard)
+  viewer:addText('Input image', text, nil, imid)
+  viewer:present()
   Script.sleep(DELAY) -- For demonstration purpose only
 
   -- Specifying the size of a square in the world (for example 166 mm / 11 squares)
@@ -56,24 +57,30 @@ local function main()
 
   -- Performing a one-shot calibration
   local cameraModel, error = Image.Calibration.Pose.estimateOneShot(checkerBoard, {squareSize}, 'COORDINATE_CODE')
-  print('Camera calibrated with average error: ' .. (math.floor(error * 10)) / 10 .. ' px')
+  print('Camera calibrated with average error: ' .. (math.floor(error * 100)) / 100 .. ' px')
 
   -- Undistort mode removes lens distortion effects, but keeps perspective
   local correction = Image.Calibration.Correction.create()
-  correction:setUndistortMode(cameraModel, 'VALID') -- Only intrinsic parameters
+  correction:setUndistortMode(cameraModel, 'VALID') -- Only intrinsic parameters, and crop to inner (no black bars)
   local correctedImage = correction:apply(checkerBoard)
-  viewer:add(correctedImage)
-  viewer:add('Undistort mode', text)
+  viewer:clear()
+  imid = viewer:addImage(correctedImage)
+  viewer:addText('Undistort mode', text, nil, imid)
   viewer:present()
   Script.sleep(DELAY) -- For demonstration purpose only
 
   -- Untilt mode removes perspective and lens distortion and converts pixels to mm
-  correction:setUntiltMode(cameraModel, 'FULL')
+  correction:setUntiltMode(cameraModel, 'FULL') -- Remove tilt, keep all pixels (black bars)
   correctedImage = correction:apply(checkerBoard)
-  viewer:add(correctedImage)
-  text:setPosition(0, 0) -- world origin
-  text:setSize(10) -- mm
-  viewer:add('Untilt mode', text)
+
+  -- From here positions are in mm
+  text:setPosition(0, squareSize)
+  text:setSize(10)
+
+  -- Display results
+  viewer:clear()
+  imid = viewer:addImage(correctedImage)
+  viewer:addText('Untilt mode', text, nil, imid)
   viewer:present()
   Script.sleep(DELAY) -- For demonstration purpose only
 
@@ -83,9 +90,11 @@ local function main()
   local worldRectangle = Shape.createRectangle(Point.create(cxy, cxy), sxy, sxy)
   correction:setAlignMode(cameraModel, worldRectangle)
   correctedImage = correction:apply(checkerBoard)
-  viewer:add(correctedImage)
-  viewer:add('Align mode', text)
+
+  imid = viewer:addImage(correctedImage)
+  viewer:addText('Align mode', text, nil, imid)
   viewer:present()
+
   print('App finished.')
 end
 --The following registration is part of the global scope which runs once after startup
